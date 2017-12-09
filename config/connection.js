ee6b0f31447a2fe0c8c716b36775c7c09fde4411
sophiasheref <sophia.sheref@gmail.com>
2017-12-09 13:54:40 -0500

    testing connection

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,30 @@ var mysql = require("mysql");
 
 var connection;
 
-// For Heroku Deployment vs. Local MySQL Database
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
-  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Xdr5Cft6",
-  database: "burgers_db"
+// mysql://mequh0cnf9leinua:og3owmh6krn1oqoi@k3xio06abqa902qt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zgaaiwayxv11e4n1
+
+
+
+connection = mysql.createConnection({
+    host: 'k3xio06abqa902qt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'mequh0cnf9leinua',
+    password: "og3owmh6krn1oqoi",
+    database: 'zgaaiwayxv11e4n1'
 });
-};
+
+
+// // For Heroku Deployment vs. Local MySQL Database
+// if(process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else{
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Xdr5Cft6",
+//   database: "burgers_db"
+// });
+// };
 
 // Make connection.
 connection.connect(function(err) {
